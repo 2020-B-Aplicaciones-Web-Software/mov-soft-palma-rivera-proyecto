6e39cd5f0dc2a190ddde92f677c1a780639c44f5
Andrés <bryan.palma02@epn.edu.ec>
2021-09-26 00:16:25 -0500

    Subir imagen a la app

--- Proyecto2B/app/src/main/java/com/example/proyecto2b/RegistroClinica.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/RegistroClinica.kt
@@ -1,12 +1,34 @@
 package com.example.proyecto2b
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 
 class RegistroClinica : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_clinica)
+        val btn=findViewById<Button>(R.id.button3)
+        btn.setOnClickListener {
+            val intent=Intent(Intent.ACTION_PICK)
+            intent.type="image/*"
+            startActivityForResult(intent,0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==0 && resultCode==Activity.RESULT_OK && data!=null){
+            val uri=data.data
+            val bitmap=MediaStore.Images.Media.getBitmap(contentResolver,uri)
+            val drawable=BitmapDrawable(bitmap)
+            findViewById<ImageView>(R.id.imageView).setImageDrawable(drawable)
+        }
     }
 }
