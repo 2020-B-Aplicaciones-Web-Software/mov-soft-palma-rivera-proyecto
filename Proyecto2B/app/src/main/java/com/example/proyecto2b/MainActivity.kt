4512c27828bb6d2c7b9434d04043f2710f878113
Andrés <bryan.palma02@epn.edu.ec>
2021-09-25 23:43:58 -0500

    Se añadio firebase

--- Proyecto2B/app/src/main/java/com/example/proyecto2b/MainActivity.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.proyecto2b
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnRegistro=findViewById<Button>(R.id.button2)
+        btnRegistro
+            .setOnClickListener {
+                startActivity(Intent(this,RegistroClinica::class.java))
+            }
     }
+
 }
