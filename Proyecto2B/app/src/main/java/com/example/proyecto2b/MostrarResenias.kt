9ec40cbf24002ac98d9650ee172917e20ce949d1
Andrés <bryan.palma02@epn.edu.ec>
2021-09-29 13:24:43 -0500

    practicamente todo

--- Proyecto2B/app/src/main/java/com/example/proyecto2b/MostrarResenias.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/MostrarResenias.kt
@@ -1,9 +1,11 @@
 package com.example.proyecto2b
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -20,13 +22,37 @@ class MostrarResenias : AppCompatActivity() {
 
         val clinica = intent.getParcelableExtra<Clinica>("CLINICA")
 
+        val barra5 = findViewById<ProgressBar>(R.id.barra5)
+        val barra4 = findViewById<ProgressBar>(R.id.barra4)
+        val barra3 = findViewById<ProgressBar>(R.id.barra3)
+        val barra2 = findViewById<ProgressBar>(R.id.barra2)
+        val barra1 = findViewById<ProgressBar>(R.id.barra1)
+        val num_resenias=findViewById<TextView>(R.id.num_resenias)
+        val promedio=findViewById<TextView>(R.id.promedio_resenia)
+
+        if (clinica != null) {
+            num_resenias.text= clinica.resenias!!.num_resenias.toString()
+            promedio.text=clinica.resenias!!.promedio.toString()
+            barra5.progress =
+                ((clinica.resenias?.num_5)?.div((clinica.resenias!!.num_resenias))!!) * 100
+            barra4.progress =
+                ((clinica.resenias?.num_4)?.div((clinica.resenias!!.num_resenias))!!) * 100
+            barra3.progress =
+                ((clinica.resenias?.num_3)?.div((clinica.resenias!!.num_resenias))!!) * 100
+            barra2.progress =
+                ((clinica.resenias?.num_2)?.div((clinica.resenias!!.num_resenias))!!) * 100
+            barra1.progress =
+                ((clinica.resenias?.num_1)?.div((clinica.resenias!!.num_resenias))!!) * 100
+        }
+
+
         val botonEscribirResenia = findViewById<Button>(R.id.btn_escribir_resenia)
         botonEscribirResenia.setOnClickListener {
             val intent = Intent(
                 this,
                 EscribirResenia::class.java
             )
-            intent.putExtra("CLINICA",clinica)
+            intent.putExtra("CLINICA", clinica)
             startActivityForResult(intent, CODIGO_RESPUESTA_INTENT_EXPLICITO)
         }
 
@@ -41,16 +67,22 @@ class MostrarResenias : AppCompatActivity() {
         referenciaResenia
             .get()
             .addOnSuccessListener { result ->
-                for (document in result){
+                for (document in result) {
                     val referencia_documento = db.collection("clinica").document(document.id)
                     referencia_documento.collection("resenias")
                         .get()
                         .addOnSuccessListener { resultado ->
-                            for (documento in resultado){
+                            for (documento in resultado) {
                                 val nombre_apellido = "${documento.data.get("nombre")}"
                                 val calificacion = "${documento.data.get("calificacion")}"
                                 val comentario = "${documento.data.get("comentario")}"
-                                arreglo_resenias.add(Resenia(nombre_apellido,calificacion.toInt(),comentario))
+                                arreglo_resenias.add(
+                                    Resenia(
+                                        nombre_apellido,
+                                        calificacion.toInt(),
+                                        comentario
+                                    )
+                                )
                             }
 
                             arregloResenias1 = arreglo_resenias
@@ -64,8 +96,10 @@ class MostrarResenias : AppCompatActivity() {
                                 recyclerViewResenias
                             )
                             recyclerViewResenias.adapter = adaptador
-                            recyclerViewResenias.itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
-                            recyclerViewResenias.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+                            recyclerViewResenias.itemAnimator =
+                                androidx.recyclerview.widget.DefaultItemAnimator()
+                            recyclerViewResenias.layoutManager =
+                                androidx.recyclerview.widget.LinearLayoutManager(this)
                             adaptador.notifyDataSetChanged()
 
                         }
@@ -76,15 +110,6 @@ class MostrarResenias : AppCompatActivity() {
 
 
     }
-
-
-
-
-
-
-
-
-
 
 
 }
--- Proyecto2B/app/src/main/java/com/example/proyecto2b/ResumenReseniasAdapter.kt
+++ Proyecto2B/app/src/main/java/com/example/proyecto2b/ResumenReseniasAdapter.kt
@@ -6,6 +6,7 @@ import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -18,7 +19,7 @@ class ResumenReseniasAdapter (
 ) : RecyclerView.Adapter<ResumenReseniasAdapter.MyViewHolder>(){
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val nombre_apellidoTextView: TextView
-        val calificacionTextView: TextView
+        val calificacionTextView: RatingBar
         val comentarioTextView: TextView
 
         init {
@@ -43,7 +44,7 @@ class ResumenReseniasAdapter (
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val resenia = listaResenias[position]
         holder.nombre_apellidoTextView.text = resenia.nombre
-        holder.calificacionTextView.text = resenia.calificacion.toString()
+        holder.calificacionTextView.rating = resenia.calificacion.toFloat()
         holder.comentarioTextView.text = resenia.descripcion
     }
 
